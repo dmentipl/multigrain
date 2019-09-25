a79b0d56ca649df5fc0e8830cf3dbc3a2534da4d
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-09-25 10:56:01 +1000

    Make setup and run more generic
    You may not want to set up multiple calculations varying only K_drag.
    This allows for setting up calculations based on a list of parameter
    files.

--- code/DUSTYBOX/dustybox_run.py
+++ code/DUSTYBOX/dustybox_run.py
@@ -8,29 +8,56 @@ sequentially.
 Daniel Mentiplay, 2019.
 """
 
+import argparse
 import pathlib
 import shutil
 import subprocess
 import sys
 
-RUN_ROOT_DIR = pathlib.Path('~/runs/multigrain/dustybox').expanduser()
-
 
 def main():
+    run_root_dir = get_command_line()
+    run(run_root_dir)
+
+
+def run(run_root_dir: pathlib.Path):
 
     print('\n' + 72 * '-')
     print('>>> Running calculations <<<')
     print(72 * '-' + '\n')
 
-    for directory in sorted(RUN_ROOT_DIR.iterdir()):
+    for directory in sorted(run_root_dir.iterdir()):
         print(f'Running {directory.name}...')
-        with open(directory / 'dustybox01.log', 'w') as fp:
+        in_files = list(directory.glob('*.in'))
+        if len(in_files) > 1:
+            raise ValueError('Too many .in files in directory')
+        in_file = in_files[0].name
+        log_file = f'{in_files[0].stem}01.log'
+        with open(directory / log_file, 'w') as fp:
             subprocess.run(
-                [directory / 'phantom', 'dustybox.in'],
-                cwd=directory,
-                stdout=fp,
-                stderr=fp,
+                [directory / 'phantom', in_file], cwd=directory, stdout=fp, stderr=fp
             )
+
+
+def get_command_line():
+    """
+    Get command line options.
+
+    Returns
+    -------
+    run_root_dir : Path
+        The path to the root directory for the calculations.
+    """
+    parser = argparse.ArgumentParser(description='Set up dustybox calculations')
+    parser.add_argument(
+        '-r',
+        '--run_root_dir',
+        help='the root directory for the calculations',
+        required=True,
+    )
+    args = parser.parse_args()
+    run_root_dir = pathlib.Path(args.run_root_dir).resolve()
+    return run_root_dir
 
 
 if __name__ == '__main__':
--- code/DUSTYBOX/dustybox_setup.py
+++ code/DUSTYBOX/dustybox_setup.py
@@ -14,39 +14,43 @@ Phantom is compiled with HDF5. So the library must be available.
 
 The following global variables are set.
 
-- RUN_ROOT_DIR
 - PHANTOM_DIR
 - REQUIRED_PHANTOM_GIT_COMMIT_HASH
 - HDF5ROOT
-- K_DRAG
 """
 
+from __future__ import annotations
+
+import argparse
 import pathlib
 import sys
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Union
+from typing import Dict, List, Union
 
 import numpy as np
-
+import tomlkit
 import phantombuild
 import phantomsetup
 
-RUN_ROOT_DIR = pathlib.Path('~/runs/multigrain/dustybox').expanduser()
 PHANTOM_DIR = pathlib.Path('~/repos/phantom').expanduser()
-
 REQUIRED_PHANTOM_GIT_COMMIT_HASH = '6666c55feea1887b2fd8bb87fbe3c2878ba54ed7'
-
 if sys.platform == 'darwin':
     HDF5ROOT = pathlib.Path('/usr/local/opt/hdf5')
 elif sys.platform == 'linux':
     HDF5ROOT = pathlib.Path('/usr/lib/x86_64-linux-gnu/hdf5/serial')
 
-K_DRAG = (0.1, 1.0, 10.0)
-
 
 @dataclass
 class Parameters:
+    """
+    Dusty box parameters.
+
+    If drag_method is set to 'Epstein/Stokes' you must set grain_size
+    and grain_density. If drag_method is set to 'K_drag' you must set
+    K_drag. Note that the length of dust_to_gas_ratio must match
+    grain_size and grain_density.
+    """
 
     prefix: str = 'dustybox'
     length_unit: str = 'au'
@@ -58,15 +62,68 @@ class Parameters:
     number_of_particles_gas: int = 50_000
     number_of_particles_dust: int = 10_000
     density_gas: float = 1.0
-    dust_to_gas_ratio: tuple = (0.01, 0.02, 0.03, 0.04, 0.05)
-    drag_method: str = 'K_const'
+    dust_to_gas_ratio: tuple = (0.01, 0.01, 0.01, 0.01, 0.01)
+    drag_method: str = 'Epstein/Stokes'
     K_drag: float = 1.0
+    grain_size: tuple = (0.01, 0.03, 0.1, 0.3, 1.0)
+    grain_density: float = 3.0
     velocity_delta: float = 1.0
     maximum_time: float = 0.1
     number_of_dumps: int = 20
 
+    def write_to_file(self, filename: Union[str, Path]) -> Parameters:
+        """
+        Write the parameters to TOML file.
+
+        Parameters
+        ----------
+        filename : str or Path
+            The name of the file to write. Should have extension
+            '.toml'.
+        """
+        document = tomlkit.document()
+        for key, value in self.__dict__.items():
+            if isinstance(value, tuple):
+                value = list(value)
+            document.add(key, value)
+        with open(filename, 'w') as fp:
+            fp.write(tomlkit.dumps(document))
+        return self
+
+    def read_from_file(self, filename: Union[str, Path]) -> Parameters:
+        """
+        Read parameters from TOML file.
+
+        Parameters
+        ----------
+        filename : str or Path
+            The name of the file to read. Should have extension '.toml'.
+
+        Returns
+        -------
+        Parameters
+            The Parameters object generated from the parameters file.
+        """
+
+        if not pathlib.Path(filename).exists():
+            raise ValueError('parameter file does not exist')
+
+        with open(filename, 'r') as fp:
+            t = tomlkit.loads(fp.read())
+
+        d = dict()
+        for key, val in t.items():
+            if isinstance(val, list):
+                val = tuple(val)
+            d[key] = val
+
+        return Parameters(**d)
+
 
 def main():
+
+    run_root_dir, parameter_files = get_command_line()
+    parameters_dict = get_parameters(parameter_files)
 
     phantombuild.get_phantom(phantom_dir=PHANTOM_DIR)
     phantombuild.checkout_phantom_version(
@@ -74,11 +131,84 @@ def main():
         required_phantom_git_commit_hash=REQUIRED_PHANTOM_GIT_COMMIT_HASH,
     )
 
-    setups = setup_all(RUN_ROOT_DIR)
+    setups = setup_all(run_root_dir, parameters_dict)
     return setups
 
 
-def setup_all(run_root_directory: pathlib.Path):
+def get_command_line():
+    """
+    Get command line options.
+
+    Returns
+    -------
+    run_root_dir : Path
+        The path to the root directory for the calculations.
+    parameter_files : List[Path]
+        The path to the parameter files.
+    """
+    parser = argparse.ArgumentParser(description='Set up dustybox calculations')
+    parser.add_argument(
+        '-r',
+        '--run_root_dir',
+        help='the root directory for the calculations',
+        required=True,
+    )
+    parser.add_argument(
+        '-p', '--parameters', nargs='+', help='a list of parameter files', required=True
+    )
+    args = parser.parse_args()
+    run_root_dir = pathlib.Path(args.run_root_dir).resolve()
+    parameter_files = [
+        pathlib.Path(parameter_file).resolve() for parameter_file in args.parameters
+    ]
+    for parameter_file in parameter_files:
+        if not parameter_file.exists():
+            raise ValueError('parameter_file does not exist')
+    return run_root_dir, parameter_files
+
+
+def get_parameters(parameter_files: List[Path]) -> Dict[str, Parameters]:
+    """
+    Get Parameter objects from parameter files.
+
+    Parameters
+    ----------
+    parameter_files
+        The list of parameter files.
+
+    Returns
+    -------
+    Dict[str, Parameters]   
+        A dictionary of Parameters. The key is the "run label" which
+        will be the sub-directory of the root directory. The value is
+        the Parameters data object for the run.
+    """
+    parameters_dict = {}
+    for parameter_file in parameter_files:
+        parameters_dict[parameter_file.stem] = Parameters().read_from_file(
+            parameter_file
+        )
+    return parameters_dict
+
+
+def setup_all(run_root_directory: pathlib.Path, parameters_dict: Dict[str, Parameters]):
+    """
+    Setup multiple calculations.
+
+    Parameters
+    ----------
+    run_root_directory
+        The path to the root directory for this series of runs.
+    parameters_dict
+        A dictionary of Parameters. The key is the "run label" which
+        will be the sub-directory of the root directory. The value is
+        the Parameters data object for the run.
+
+    Returns
+    -------
+    List[phantomsetup.Setup]
+        A list of Setup objects.
+    """
 
     print('\n' + 72 * '-')
     print('>>> Setting up calculations <<<')
@@ -88,16 +218,10 @@ def setup_all(run_root_directory: pathlib.Path):
         run_root_directory.mkdir(parents=True)
 
     setups = list()
-    for K_drag in K_DRAG:
-        run_label = f'K={K_drag}'
+    for run_label, parameters in parameters_dict.items():
         print(f'Setting up {run_label}...')
-
-        maximum_time = 0.1 / K_drag
-        parameters = Parameters(K_drag=K_drag, maximum_time=maximum_time)
-
         run_directory = run_root_directory / run_label
         run_directory.mkdir()
-
         setups.append(
             setup_dustybox(parameters=parameters, run_directory=run_directory)
         )
@@ -108,6 +232,20 @@ def setup_all(run_root_directory: pathlib.Path):
 def setup_dustybox(
     parameters: Parameters, run_directory: Union[str, Path]
 ) -> phantomsetup.Setup:
+    """
+    Setup a Phantom dustybox calculation.
+
+    Parameters
+    ----------
+    parameters
+        The parameters for this calculation.
+    run_directory
+        The path to the directory containing the run.
+
+    Returns
+    -------
+    phantomsetup.Setup
+    """
 
     # Constants
     igas = phantomsetup.defaults.PARTICLE_TYPE['igas']
@@ -133,12 +271,22 @@ def setup_dustybox(
     density_dust = [
         eps * parameters.density_gas for eps in parameters.dust_to_gas_ratio
     ]
-    setup.set_dust(
-        dust_method='largegrains',
-        drag_method=parameters.drag_method,
-        drag_constant=parameters.K_drag,
-        number_of_dust_species=number_of_dust_species,
-    )
+    if parameters.drag_method == 'Epstein/Stokes':
+        setup.set_dust(
+            dust_method='largegrains',
+            drag_method='Epstein/Stokes',
+            grain_size=parameters.grain_size,
+            grain_density=parameters.grain_density,
+        )
+    elif parameters.drag_method == 'K_drag':
+        setup.set_dust(
+            dust_method='largegrains',
+            drag_method='K_drag',
+            drag_constant=parameters.K_drag,
+            number_of_dust_species=number_of_dust_species,
+        )
+    else:
+        raise ValueError('Cannot set up dust')
 
     setup.set_boundary(parameters.box_boundary, periodic=True)
 
@@ -189,4 +337,4 @@ def setup_dustybox(
 
 
 if __name__ == '__main__':
-    setups = main()
+    main()

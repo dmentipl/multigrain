addd78dea4ed332b6ca9767f789d6a006f1f2ee0
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-02-12 15:58:15 +1100

    Fix for previous commits

--- code/modules/multigrain/__init__.py
+++ code/modules/multigrain/__init__.py
@@ -1,17 +1,5 @@
 """Test problems for multigrain paper."""
 
-from . import dustybox, dustywave, dustyshock
-from .run import setup_multiple_calculations, run_multiple_calculations
-
-__all__ = [
-    'dustybox',
-    'dustywave',
-    'dustyshock',
-    'setup_multiple_calculations',
-    'run_multiple_calculations',
-]
-
-
 # Required Phantom version.
 PHANTOM_VERSION = 'e0d6986df99d980267f712d2218376dd50701117'
 
@@ -20,3 +8,6 @@ HDF5ROOT = '/usr/local/opt/hdf5'
 
 # Extra compiler arguments.
 EXTRA_COMPILER_ARGUMENTS = ['FC=gfortran-9']
+
+from . import dustybox, dustywave, dustyshock
+from .run import setup_multiple_calculations, run_multiple_calculations

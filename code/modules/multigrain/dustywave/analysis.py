86b1cc8d542ccfa594c7eb5e7dafeef9238b765b
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-04-23 11:51:34 +1000

    Small fix

--- code/modules/multigrain/dustywave/analysis.py
+++ code/modules/multigrain/dustywave/analysis.py
@@ -48,9 +48,6 @@ def calculate_velocity_density(
     return pd.DataFrame(arrays, columns=columns)
 
 
-########################################################################################
-
-
 def plot_velocity_density(dataframes):
 
     fig, axs = plt.subplots(ncols=len(dataframes), nrows=2, sharex=True, figsize=(8, 6))
@@ -75,73 +72,3 @@ def plot_velocity_density(dataframes):
     axs[1, 0].set(ylabel='Normalized density')
 
     return fig
-
-
-########################################################################################
-
-
-def a():
-    t_range = 0.0, 2.0
-    rho_range = -1, 1
-    vx_range = -1, 1
-
-    ps1 = list()
-    ps2 = list()
-
-    colors = Category10[5]
-
-    for run, df in dataframes.items():
-
-        n_species = len([col for col in df.columns if col.startswith('rho')])
-
-        p1 = figure(
-            title=f"Normalized density perturbation at x=0 for {run}",
-            x_range=t_range,
-            y_range=rho_range,
-            plot_width=400,
-            plot_height=300,
-        )
-        p2 = figure(
-            title=f"Normalized velocity perturbation at x=0 for {run}",
-            x_range=t_range,
-            y_range=vx_range,
-            plot_width=400,
-            plot_height=300,
-        )
-
-        x = np.array(df["time"])
-
-        for idx in range(n_species):
-            legend_label = f"dust {idx}" if idx != 0 else "gas"
-
-            y_numerical = np.array(df[f"rho.{idx}"])
-            if idx == 0:
-                y_exact = rho_g(time=x, omega=omega[n_species])
-            else:
-                y_exact = rho_d(
-                    time=x, omega=omega[n_species], tstop=tstop[n_species][idx - 1]
-                )
-
-            p1.line(x, y_exact, line_color=colors[idx])
-            p1.circle(
-                x, y_numerical, line_color=colors[idx], fill_color=None
-            )  # , legend_label=legend_label
-
-            y_numerical = np.array(df[f"vx.{idx}"])
-            if idx == 0:
-                y_exact = v_g(time=x, omega=omega[n_species])
-            else:
-                y_exact = v_d(
-                    time=x, omega=omega[n_species], tstop=tstop[n_species][idx - 1]
-                )
-
-            p2.line(x, y_exact, line_color=colors[idx])
-            p2.circle(
-                x, y_numerical, line_color=colors[idx], fill_color=None
-            )  # , legend_label=legend_label
-
-        ps1.append(p1)
-        ps2.append(p2)
-
-    grid = gridplot([ps2[1], ps2[0], ps1[1], ps1[0]], ncols=2)
-    show(grid)

61839a00b5f3766e6d4ffbdd7b7db29937a4305a
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-05-15 16:42:46 +1000

    Allow for multiple runs in setup scripts

--- code/scripts/dustyshock_compile_phantom.py
+++ code/scripts/dustyshock_compile_phantom.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 from os import getenv
 from pathlib import Path
+from typing import Tuple
 
 import click
 import phantombuild
@@ -13,6 +14,7 @@ HDF5_DIR = getenv('HDF5_DIR')
 CODE_DIR = Path('~/repos/multigrain/code').expanduser()
 IC_DIR = CODE_DIR / 'initial-conditions' / 'dustyshock'
 SLURM_FILE = CODE_DIR / 'misc' / 'dustyshock-slurm.swm'
+SETUP = 'dustyshock'
 
 PHANTOM_DIR = '~/repos/phantom'
 PHANTOM_VERSION = 'd9a5507f3fd97b5ed5acf4547f82449476b29091'
@@ -24,7 +26,12 @@ PHANTOM_PATCHES = [
 
 
 @click.command()
-@click.option('--run_name', required=True, help='The name of the run.')
+@click.option(
+    '--run_name',
+    multiple=True,
+    required=True,
+    help='The name of the run. Can be multiple, one name per run.',
+)
 @click.option(
     '--root_dir', required=True, help='The directory in which to put run directory.'
 )
@@ -37,7 +44,7 @@ PHANTOM_PATCHES = [
 @click.option('--fortran_compiler', required=False, help='The Fortran compiler.')
 @click.option('--schedule_job', required=False, help='Schedule the run via Slurm.')
 def main(
-    run_name: str,
+    run_name: Tuple[str],
     root_dir: str,
     system: str,
     equation_of_state: str,
@@ -70,22 +77,28 @@ def main(
         extra_makefile_options['FC'] = fortran_compiler
     phantombuild.build_phantom(
         phantom_dir=PHANTOM_DIR,
-        setup='dustyshock',
+        setup=SETUP,
         system=system,
         hdf5_location=HDF5_DIR,
         extra_makefile_options=extra_makefile_options,
     )
 
-    # Set up calculation
-    run_dir = Path(root_dir).expanduser() / run_name
-    input_dir = IC_DIR / run_name
-    phantombuild.setup_calculation(
-        prefix=PREFIX, run_dir=run_dir, input_dir=input_dir, phantom_dir=PHANTOM_DIR,
-    )
+    # Loop over run names
+    for _run_name in run_name:
 
-    # Schedule calculation
-    if schedule_job:
-        _schedule(run_dir=run_dir)
+        # Set up calculation
+        run_dir = Path(root_dir).expanduser() / _run_name
+        input_dir = IC_DIR / _run_name
+        phantombuild.setup_calculation(
+            prefix=PREFIX,
+            run_dir=run_dir,
+            input_dir=input_dir,
+            phantom_dir=PHANTOM_DIR,
+        )
+
+        # Schedule calculation
+        if schedule_job:
+            _schedule(run_dir=run_dir)
 
 
 def _schedule(run_dir: Path):
--- code/scripts/radialdrift_compile_phantom.py
+++ code/scripts/radialdrift_compile_phantom.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 from os import getenv
 from pathlib import Path
+from typing import Tuple
 
 import click
 import phantombuild
@@ -13,6 +14,7 @@ HDF5_DIR = getenv('HDF5_DIR')
 CODE_DIR = Path('~/repos/multigrain/code').expanduser()
 IC_DIR = CODE_DIR / 'initial-conditions' / 'radialdrift'
 SLURM_FILE = CODE_DIR / 'misc' / 'radialdrift-slurm.swm'
+SETUP = 'dustydisc'
 
 PHANTOM_DIR = '~/repos/phantom'
 PHANTOM_VERSION = 'd9a5507f3fd97b5ed5acf4547f82449476b29091'
@@ -24,7 +26,13 @@ PHANTOM_PATCHES = [
 
 
 @click.command()
-@click.option('--run_name', required=True, help='The name of the run.')
+@click.command()
+@click.option(
+    '--run_name',
+    multiple=True,
+    required=True,
+    help='The name of the run. Can be multiple, one name per run.',
+)
 @click.option(
     '--root_dir', required=True, help='The directory in which to put run directory.'
 )
@@ -32,7 +40,7 @@ PHANTOM_PATCHES = [
 @click.option('--fortran_compiler', required=False, help='The Fortran compiler.')
 @click.option('--schedule_job', required=False, help='Schedule the run via Slurm.')
 def main(
-    run_name: str,
+    run_name: Tuple[str],
     root_dir: str,
     system: str,
     fortran_compiler: str = None,
@@ -57,22 +65,28 @@ def main(
         extra_makefile_options['FC'] = fortran_compiler
     phantombuild.build_phantom(
         phantom_dir=PHANTOM_DIR,
-        setup='dustydisc',
+        setup=SETUP,
         system=system,
         hdf5_location=HDF5_DIR,
         extra_makefile_options=extra_makefile_options,
     )
 
-    # Set up calculation
-    run_dir = Path(root_dir).expanduser() / run_name
-    input_dir = IC_DIR / run_name
-    phantombuild.setup_calculation(
-        prefix=PREFIX, run_dir=run_dir, input_dir=input_dir, phantom_dir=PHANTOM_DIR,
-    )
+    # Loop over run names
+    for _run_name in run_name:
 
-    # Schedule calculation
-    if schedule_job:
-        _schedule(run_dir=run_dir)
+        # Set up calculation
+        run_dir = Path(root_dir).expanduser() / _run_name
+        input_dir = IC_DIR / _run_name
+        phantombuild.setup_calculation(
+            prefix=PREFIX,
+            run_dir=run_dir,
+            input_dir=input_dir,
+            phantom_dir=PHANTOM_DIR,
+        )
+
+        # Schedule calculation
+        if schedule_job:
+            _schedule(run_dir=run_dir)
 
 
 def _schedule(run_dir: Path):

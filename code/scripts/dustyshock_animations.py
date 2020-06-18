19b7cf9c77fccfd6d43b4184502d4cc99193f011
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-06-18 11:16:58 +1000

    Tidy up animation script

--- code/scripts/dustyshock_animations.py
+++ code/scripts/dustyshock_animations.py
@@ -16,35 +16,36 @@ YLIMS_3 = [(-0.4, 2.8), (-2.0, 22.0)]
 
 
 def main():
-    loop_over_sims(path=PATH_1, glob=NAME_GLOB, ys=YS, xlim=XLIM, ylims=YLIMS_1)
-    loop_over_sims(path=PATH_3, glob=NAME_GLOB, ys=YS, xlim=XLIM, ylims=YLIMS_3)
+    for path in PATH_1.glob(NAME_GLOB):
+        particle_animation(path, YS, XLIM, YLIMS_1)
+    for path in PATH_3.glob(NAME_GLOB):
+        particle_animation(path, YS, XLIM, YLIMS_3)
 
 
-def loop_over_sims(path, glob, ys, xlim, ylims):
-    for p in path.glob(glob):
-        print(f'Simulation: {p.name}')
-        name = (
-            p.name.replace('smooth_fac', 'smooth^fac')
-            .replace('_', '=')
-            .replace('-', ', ')
-            .replace('smooth^fac', 'smooth_fac')
-        )
-        filename = p / 'anim.mp4'
+def particle_animation(directory, ys, xlim, ylims):
+    print(f'Simulation: {directory.name}')
+    name = (
+        directory.name.replace('smooth_fac', 'smooth^fac')
+        .replace('_', '=')
+        .replace('-', ', ')
+        .replace('smooth^fac', 'smooth_fac')
+    )
+    filename = directory / 'anim.mp4'
 
-        print('Loading simulation...')
-        sim = plonk.load_sim(prefix='dustyshock', directory=p)
+    print('Loading simulation...')
+    sim = plonk.load_sim(prefix='dustyshock', directory=directory)
 
-        print('Loading snaps...')
-        snaps = sim.snaps
+    print('Loading snaps...')
+    snaps = sim.snaps
 
-        print('Animating...')
-        particle_animation(
-            name=name, filename=filename, snaps=snaps, ys=ys, xlim=xlim, ylims=ylims
-        )
-        print('Finished!')
+    print('Animating...')
+    _particle_animation(
+        name=name, filename=filename, snaps=snaps, ys=ys, xlim=xlim, ylims=ylims
+    )
+    print('Finished!')
 
 
-def particle_animation(name, filename, snaps, ys, xlim, ylims):
+def _particle_animation(name, filename, snaps, ys, xlim, ylims):
     figsize = (9, 3 * len(ys))
     fig, axs = plt.subplots(nrows=len(ys), sharex=True, figsize=figsize)
     fig.subplots_adjust(hspace=0.05)

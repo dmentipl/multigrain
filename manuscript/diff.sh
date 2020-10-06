7fa9acfb704faab7b5e2dc2fc7e27cb32a67a154
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-10-07 10:58:45 +1100

    Fix bugs in diff.sh script

--- manuscript/diff.sh
+++ manuscript/diff.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 #
-# Make a latexdiff of the current manuscript
+# Make a latexdiff of the current manuscript.
+#
+# NOTES:
+# - Must be run from a directory inside the git repo.
+# - Produces a file "diff.pdf" containing the diff.
+# - Removes blue highlighting in MNRAS links
+#   (which conflicts with blue highlighting from diff).
 
-# This is the submitted commit hash
-# Edit if you need a diff to another commit
+################################################################################
+# EDIT AS REQUIRED
+################################################################################
+
+# DIR is the directory in which the manuscript lives.
+# This must be specified relative to the git repository root.
+DIR=manuscript
+
+# FILENAME is the manuscript filename.
+FILENAME=multigrain-paper.tex
+
+# COMMIT is the commit hash of the earlier version.
+# Note: get the commit hash of a TAG with:
+# $ git rev-parse TAG
 COMMIT=e4b704197b882c6f49560ebbea9f0aabbd844e9e
-# Note: get the commit hash of a tag with:
-#   git rev-parse TAG
 
-git show ${COMMIT}:manuscript/multigrain-paper.tex > multigrain-paper_previous.tex
-latexdiff multigrain-paper_previous.tex multigrain-paper.tex > multigrain-paper_diff.tex
-python diff.py
-python << END
-f = open("multigrain-paper_diff.tex", "r")
-contents = f.readlines()
-f.close()
-contents.insert(44, "\hypersetup{colorlinks=false}")
-f = open("multigrain-paper_diff.tex", "w")
-contents = "".join(contents)
-f.write(contents)
-f.close()
-END
-latexmk multigrain-paper_diff.tex
-latexmk -c multigrain-paper_diff.tex
-rm multigrain-paper_diff.tex multigrain-paper_previous.tex
+################################################################################
+# DO NOT EDIT BELOW
+################################################################################
+
+cd $(git rev-parse --show-toplevel)/${DIR} || return
+git show ${COMMIT}:${DIR}/${FILENAME} > previous.tex
+latexdiff previous.tex ${FILENAME} > diff.tex
+rm previous.tex
+(( LN=$(grep -n 'documentclass' diff.tex | cut -f1 -d:) + 1 ))
+awk -v n=${LN} -v s="\\\hypersetup{colorlinks=false}" 'NR == n {print s} {print}' diff.tex > tmp
+mv tmp diff.tex
+latexmk diff.tex
+latexmk -c diff.tex
+rm diff.tex
+cd - || return
